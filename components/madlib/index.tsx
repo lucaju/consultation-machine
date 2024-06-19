7ce7ce6dc7c83e253e01285941ebf0bdc59d4216
Luciano Frizzera <lucaju@me.com>
2024-06-19 11:12:27 -0400

    fix: 🐛 display the prompt without the headings

--- components/madlib/index.tsx
+++ components/madlib/index.tsx
@@ -1,23 +1,30 @@
 'use client';
 
 import { madlibStructureEN, madlibStructureFR } from '@/jotai/intiial-data';
-import { madlibAtom } from '@/jotai/store';
+import { madlibAtom, madlibReadyAtom } from '@/jotai/store';
 import { Flex } from '@radix-ui/themes';
-import { useSetAtom } from 'jotai';
+import { useAtomValue, useSetAtom } from 'jotai';
 import { useLocale } from 'next-intl';
+import { useEffect } from 'react';
 import { Form } from './form';
 import { Result } from './result';
 
 export const Madlib = () => {
   const locale = useLocale();
   const madlib = locale === 'en' ? madlibStructureEN : madlibStructureFR;
+
   const setMadlibReady = useSetAtom(madlibAtom);
-  setMadlibReady(madlib);
+  const madlibReady = useAtomValue(madlibReadyAtom);
+
+  useEffect(() => {
+    setMadlibReady(madlib);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <Flex direction="column" gap="2" align="center">
-      <Form />
-      <Result />
+    <Flex direction="column" gap="4" align="center">
+      {madlib.length > 0 && <Form />}
+      {madlibReady && <Result />}
     </Flex>
   );
 };
--- components/madlib/result.tsx
+++ components/madlib/result.tsx
@@ -1,45 +1,35 @@
 'use client';
 
-import { madlibAtom, madlibReadyAtom } from '@/jotai/store';
+import { madlibAtom } from '@/jotai/store';
 import { Box, Flex, Heading, Text } from '@radix-ui/themes';
 import { useAtomValue } from 'jotai';
 
 export const Result = () => {
   const madlib = useAtomValue(madlibAtom);
-  const madlibReady = useAtomValue(madlibReadyAtom);
 
   return (
     <Flex direction="column" gap="2" align="center">
-      {madlibReady && (
-        <Box
-          p="4"
-          style={{
-            backgroundColor: 'var(--gray-2)',
-            borderRadius: 'var(--radius-3)',
-            boxShadow: 'var(--shadow-2)',
-          }}
-          width="600"
-        >
-          {madlib.map((item, index) =>
-            item.type === 'heading' ? (
-              <Heading key={item.id} as="h6" size="4" style={{ marginTop: 12 }}>
-                {item.value}
-              </Heading>
-            ) : item.type === 'text' ? (
-              <Text key={item.id}>
-                {madlib[index - 1].value.endsWith('.') ? ' ' : ''}
+      <Heading size="5">Prompt</Heading>
+      <Box
+        p="4"
+        style={{
+          backgroundColor: 'var(--gray-2)',
+          borderRadius: 'var(--radius-3)',
+          boxShadow: 'var(--shadow-2)',
+        }}
+        width="600"
+      >
+        {madlib.map(
+          (item, index) =>
+            item.type !== 'heading' && (
+              <Text key={item.id} style={{ fontWeight: item.type === 'text' ? 'normal' : 'bold' }}>
+                {madlib[index - 1]?.value.endsWith('.') ? ' ' : ''}
                 {item.value.trim()}
-              </Text>
-            ) : (
-              <Text key={item.id} style={{ fontWeight: 'bold' }}>
-                {madlib[index - 1].value.endsWith(' ') ? '' : ' '}
-                {item.value.trim()}
-                {madlib[index + 1].value.startsWith('.') ? '' : ' '}
+                {madlib[index + 1]?.value.startsWith('.') ? '' : ' '}
               </Text>
             ),
-          )}
-        </Box>
-      )}
+        )}
+      </Box>
     </Flex>
   );
 };
