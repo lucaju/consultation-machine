3cc9e0ed4616477427c0a909e395d4b9cbfc59a2
Luciano Frizzera <lucaju@me.com>
2024-06-19 11:13:13 -0400

    feat: 🎸 add feature: edit and copy

--- components/llm/letter.tsx
+++ components/llm/letter.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { llmResultAtom, madlibAtom } from '@/jotai/store';
-import { Box, Button, Section, Text } from '@radix-ui/themes';
+import { Box, Button, Flex, Heading, Section, Text, Tooltip } from '@radix-ui/themes';
 import { format } from 'date-fns';
 import { useAtomValue } from 'jotai';
 import { useTranslations } from 'next-intl';
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useState } from 'react';
+import { LiaCheckSolid, LiaEdit } from 'react-icons/lia';
+import { PiCopySimple, PiPrinter } from 'react-icons/pi';
 import { useReactToPrint } from 'react-to-print';
 
 export const Letter = () => {
@@ -14,7 +16,9 @@ export const Letter = () => {
   const llmResult = useAtomValue(llmResultAtom);
   const madlib = useAtomValue(madlibAtom);
 
-  const componentRef = useRef(null);
+  const componentRef = useRef<HTMLDivElement | null>(null);
+
+  const [editable, setEditable] = useState(false);
 
   const personNameItem = madlib.find((item) => 'name' in item && item.name === 'person_name');
   const personName = personNameItem?.value ?? 'Anonymous';
@@ -40,53 +44,99 @@ export const Letter = () => {
     removeAfterPrint: true,
   });
 
-  return (
-    <Box
-      ref={componentRef}
-      p="5"
-      px="9"
-      style={{
-        backgroundColor: 'var(--gray-a2)',
-        borderRadius: 'var(--radius-3)',
-        boxShadow: 'var(--shadow-4)',
-        width: 1000,
-      }}
-      minWidth="600"
-    >
-      <Button onClick={handlePrint}>print</Button>
-      <Section py="3" style={{ width: 400 }}>
-        <Text as="p" style={{ marginBottom: 16 }}>
-          {personName}
-        </Text>
-        <Text as="p" weight="bold" style={{ marginBottom: 16 }}>
-          {t('project.BUSINESS CONFIDENTIAL')}
-        </Text>
-        <Text as="p" style={{ marginBottom: 16 }}>
-          {today}
-        </Text>
-        <Text as="p" style={{ marginBottom: 16 }}>
-          <Text>{t('project.The Honourable Joel Lightbound MP')}</Text>
-          <br />
-          <Text>{t('project.Chair House of Commons Standing')}</Text>
-          <br />
-          <Text>{t('project.Committee on Industry and Technology')}</Text>
-          <br />
-          <Text>{t('project.House of Commons')}</Text>
-          <br />
-          <Text>{t('project.Ottawa Ontario K1A 0A6')}</Text>
-        </Text>
-      </Section>
+  const handleCopy = async () => {
+    if (!componentRef.current) return;
 
-      <Section py="3">
-        {parseText?.map((item, index) => (
-          <Text as="p" key={index} style={{ marginBottom: 16 }}>
-            {item}
+    const html = componentRef.current.outerHTML;
+    if (!html) return;
+
+    const htmlBlob = new Blob([html], { type: 'text/html' });
+
+    const text = componentRef.current.textContent ?? '';
+    const textBlob = new Blob([text], { type: 'text/plain' });
+
+    const clipboardItem = new ClipboardItem({
+      [htmlBlob.type]: htmlBlob,
+      [textBlob.type]: textBlob,
+    });
+
+    return navigator.clipboard.write([clipboardItem]);
+  };
+
+  const handleEditable = () => {
+    setEditable(!editable);
+  };
+
+  return (
+    <Flex direction="column" gap="2" align="center">
+      <Heading size="3">{t('project.Letter')}</Heading>
+      <Flex direction="row" justify="end" gap="2">
+        <Tooltip content={t('project.Edit')}>
+          <Button color="gray" onClick={handleEditable} variant="outline">
+            {editable ? <LiaCheckSolid /> : <LiaEdit />}
+          </Button>
+        </Tooltip>
+        <Tooltip content={t('project.Copy')}>
+          <Button color="gray" onClick={handleCopy} variant="outline">
+            <PiCopySimple />
+          </Button>
+        </Tooltip>
+        <Tooltip content={t('project.Print')}>
+          <Button color="gray" onClick={handlePrint} variant="outline">
+            <PiPrinter />
+          </Button>
+        </Tooltip>
+      </Flex>
+      <Box
+        ref={componentRef}
+        p="5"
+        px="9"
+        style={{
+          backgroundColor: 'var(--gray-a2)',
+          borderRadius: 'var(--radius-3)',
+          boxShadow: 'var(--shadow-4)',
+          width: 1000,
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: editable ? 'var(--iris-10)' : 'var(--gray-a3)',
+        }}
+        minWidth="600"
+        contentEditable={editable ? 'plaintext-only' : 'false'}
+      >
+        <Section py="3" style={{ width: 400 }}>
+          <Text as="p" style={{ marginBottom: 16 }}>
+            {personName}
           </Text>
-        ))}
-        <Text as="p" style={{ marginBottom: 16 }}>
-          {personName}
-        </Text>
-      </Section>
-    </Box>
+          <Text as="p" weight="bold" style={{ marginBottom: 16 }}>
+            {t('project.BUSINESS CONFIDENTIAL')}
+          </Text>
+          <Text as="p" style={{ marginBottom: 16 }}>
+            {today}
+          </Text>
+          <Text as="p" style={{ marginBottom: 16 }}>
+            <Text>{t('project.The Honourable Joel Lightbound MP')}</Text>
+            <br />
+            <Text>{t('project.Chair House of Commons Standing')}</Text>
+            <br />
+            <Text>{t('project.Committee on Industry and Technology')}</Text>
+            <br />
+            <Text>{t('project.House of Commons')}</Text>
+            <br />
+            <Text>{t('project.Ottawa Ontario K1A 0A6')}</Text>
+          </Text>
+        </Section>
+
+        <Section py="3">
+          {parseText?.map((item, index) => (
+            <Text as="p" key={index} style={{ marginBottom: 16 }}>
+              {item}
+            </Text>
+          ))}
+          <Text as="p" style={{ marginBottom: 16 }}>
+            {personName}
+          </Text>
+        </Section>
+      </Box>
+    </Flex>
   );
 };
